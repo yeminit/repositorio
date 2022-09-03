2b3a50800f501148891b60a361c018f92509ab3f
Tatiana Yemini <tatianayemini@gmail.com>
2022-09-02 21:34:19 -0300

    entrega 2 completa

--- js/products.js
+++ js/products.js
@@ -2,7 +2,11 @@ const PRODUCT_AUTO_URL = "https://japceibal.github.io/emercado-api/cats_products
 
 let productos = [];
 
+let preciomin = undefined;
+let preciomax = undefined;
+
 function categorias(autos){
+    document.getElementById("paraprod").innerHTML ="";
     let listaAutos = "";
     for(let auto of autos) {
         
@@ -28,14 +32,61 @@ function categorias(autos){
 document.getElementById("paraprod").innerHTML=listaAutos ;
 }
 
+function filtrar(){
+    
+    preciomin = parseInt(document.getElementById("min").value);
+    preciomax = parseInt(document.getElementById("max").value);
+    //let listaFiltrada = productos.filter((producto) =>  {if ((!(parseInt(producto.cost) < preciomin) && (!(parseInt(producto.cost) > preciomax)))) {
+    let listaFiltrada = productos.filter((producto) => producto.cost >= preciomin && producto.cost <= preciomax);
+    categorias(listaFiltrada);  
+    console.log(listaFiltrada);
+    }
+
+
 let tomacategoria = localStorage.getItem("catID");
 
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCTS_URL+tomacategoria+".json").then(function(resultObj){
         if (resultObj.status === "ok")
             {
-                productos = resultObj.data;
-                categorias(productos.products);
+                productos = resultObj.data.products;
+                categorias(productos);
             }
+    }); 
+    document.getElementById("botonfiltro").addEventListener("click",()=>{
+        filtrar();    
+    });
+    document.getElementById("sortAsc").addEventListener("click",()=>{
+        productos.sort((a,b)=> {
+            return (parseInt(a.cost) - parseInt(b.cost));
+        });
+        categorias(productos);
+    });
+    document.getElementById("sortDesc").addEventListener("click",()=>{
+        productos.sort((a,b)=> {
+            return (parseInt(b.cost) - parseInt(a.cost));
+         });
+        categorias(productos);
+    });
+    
+    document.getElementById("sortByCount").addEventListener("click",()=>{
+        productos.sort((a,b)=> {
+            return (parseInt(b.soldCount) - parseInt(a.soldCount));
+         });
+        categorias(productos);
+    });
+    document.getElementById("botonlimpia").addEventListener("click",()=>{
+        document.getElementById("min").value = "";
+        document.getElementById("max").value = "";
+        preciomin = undefined;
+        preciomax = undefined;
+        categorias(productos);
     });
 });
+
+
+
+
+
+
+
